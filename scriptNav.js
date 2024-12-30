6d826f48cbc68360489c549c03c0072e060c7eb1
Alexandre Beduc <alexandre.beduc@gmail.com>
2024-12-30 02:06:57 +0100

    Frontend of index.html v0.0.1

---
+++ scriptNav.js
@@ -0,0 +1,14 @@
+let lastScrollTop = 0;
+const navbar = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+    const scrollTop = document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        navbar.classList.add('hidden');
+    } else {
+        navbar.classList.remove('hidden');
+    }
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+});
